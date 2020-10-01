b24d6202dc974619a22b3af4e3fc7969df7625ce
Adriano Rocha de Oliveira <adriano.rocha.oliveira@hotmail.com>
2020-10-01 15:57:45 -0300

    Implementação do Footer

--- HoldingHands-Front/src/app/footer/footer.component.ts
+++ HoldingHands-Front/src/app/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+
+  faInstagram = faInstagram
+  faGithub = faGithub
+  
 
   constructor() { }
 
