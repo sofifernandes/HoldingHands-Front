01ca721fa89e8d430b7fa9d7fd4efb97d2c14e69
Adriano Rocha de Oliveira <adriano.rocha.oliveira@hotmail.com>
2020-10-05 11:17:06 -0300

    Ligação do Front com BD

---
+++ HoldingHands-Front/src/app/model/Postagem.ts
@@ -0,0 +1,13 @@
+import { Tema } from './Tema';
+import { User } from './User';
+
+export class Postagem {
+  public id: number
+  public titulo: string
+  public textoPostagem: string
+  public data: Date
+  public tema: Tema
+  public qntCurtidas: number
+  public tipoPostagem: string
+  public usuario: User
+}
---
+++ HoldingHands-Front/src/app/service/auth.service.ts
@@ -0,0 +1,44 @@
+import { UserLogin } from './../model/UserLogin';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../model/User';
+import { environment } from 'src/environments/environment.prod';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  logar(userLogin: UserLogin) {
+    return this.http.post('http://localhost:8080/usuario/logar', userLogin)
+  }
+
+  cadastrar(user: User) {
+    return this.http.post('http://localhost:8080/usuario/cadastrar', user)
+  }
+
+  btnSair() {
+    let ok = false
+    let token = localStorage.getItem("token")
+
+    if (token != null) {
+      ok = true
+    }
+
+    return ok
+  }
+
+  btnLogin() {
+    let ok = false
+    let token = localStorage.getItem("token")
+
+    if (token == null) {
+      ok = true
+    }
+
+    return ok
+  }
+
+}
