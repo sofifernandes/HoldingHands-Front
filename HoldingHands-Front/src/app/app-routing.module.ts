7a721da064135662e26be748b572d7e28073dbd3
Adriano Rocha de Oliveira <adriano.rocha.oliveira@hotmail.com>
2020-10-09 13:53:25 -0300

    Implementação Put e Delete

--- HoldingHands-Front/src/app/app-routing.module.ts
+++ HoldingHands-Front/src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { ContatoComponent } from './contato/contato.component';
+import { DeletePostagemComponent } from './delete-postagem/delete-postagem.component';
+import { DeleteTemaComponent } from './delete-tema/delete-tema.component';
 import { FeedComponent } from './feed/feed.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { PerfilComponent } from './perfil/perfil.component';
 import { PostTemaComponent } from './post-tema/post-tema.component';
+import { PutPostagemComponent } from './put-postagem/put-postagem.component';
+import { PutTemaComponent } from './put-tema/put-tema.component';
 import { SobreComponent } from './sobre/sobre.component';
 
 
@@ -19,8 +23,11 @@ const routes: Routes = [
   {path: 'cadastro', component: CadastroComponent },
   {path: 'cadastro-tema', component: PostTemaComponent},
   {path: 'feed', component: FeedComponent},
-  {path: 'perfil', component: PerfilComponent}
-
+  {path: 'perfil', component: PerfilComponent},
+  {path: 'editar-post/:id', component: PutPostagemComponent},
+  {path: 'delete-post/:id', component: DeletePostagemComponent},
+  {path: 'editar-tema/:id', component: PutTemaComponent},
+  {path: 'delete-post/:id', component: DeleteTemaComponent}
 ];
 
 @NgModule({
